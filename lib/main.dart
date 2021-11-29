d84a7be407bdca36399fc70c082329789ebb56b2
samuelskinstad <s_skinstad@hotmail.com>
2021-11-29 15:30:45 +0100

    Removed debug on appbar

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TIG169 TODO',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
